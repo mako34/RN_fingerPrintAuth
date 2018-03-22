0bdaaa9f98d8692d4b2c48eee2fb8a4867b21547
Manuel Betancurt <manuel.betancurt@smokeball.com>
2018-03-22 15:36:22 +1100

    prima nocte, example of fingerprinth reading from lib

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import Application from './src/Application.container';
+
+AppRegistry.registerComponent('huella1', () => Application);
+// AppRegistry.registerComponent('huella1', () => App);
